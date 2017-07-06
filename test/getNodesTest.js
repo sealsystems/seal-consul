07c7818c8c813f9ba2ba3a13b7f0133d6f13279c
seal-mt <manfred.thein@sealsystems.de>
2017-07-06 11:50:58 +0200

    added a test for getNodes

--- test/getNodesTest.js
+++ test/getNodesTest.js
@@ -1,12 +1,35 @@
 'use strict';
 
 const assert = require('assertthat');
+const host = require('docker-host')().host;
+const uuid = require('uuidv4');
 
+const consul = require('../lib/consul');
 const getNodes = require('../lib/getNodes');
 
 suite('getNodes', () => {
+  const serviceName = uuid();
+
+  suiteSetup((done) => {
+    consul.connect({
+      consulUrl: `http://${host}:8500`,
+      serviceName,
+      serviceUrl: `http://${host}:2999`
+    }, done);
+  });
+
   test('is a function.', (done) => {
     assert.that(getNodes).is.ofType('function');
     done();
+  });
+
+  test('test', (done) => {
+    consul.getNodes({ service: serviceName }, (errNodes, nodes) => {
+      assert.that(errNodes).is.falsy();
+      assert.that(nodes).is.not.falsy();
+      assert.that(nodes).is.ofType('array');
+      assert.that(nodes.length).is.greaterThan(0);
+      done();
+    });
   });
 });
