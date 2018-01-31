e99efcce783621872342027bd2bf2ea450eebfeb
Golo Roden <golo.roden@thenativeweb.io>
2018-01-31 15:26:44 +0100

    Rewrite watch tests.

--- test/watch/kvTest.js
+++ test/watch/kvTest.js
@@ -9,63 +9,53 @@ const consul = require('../../lib/consul');
 const kvTree = require('../../lib/watch/kv');
 
 suite('watch/kv', () => {
-  test('is a function.', (done) => {
+  test('is a function.', async () => {
     assert.that(kvTree).is.ofType('function');
-    done();
   });
 
-  test('throws an error if options are missing.', (done) => {
-    assert.that(() => {
-      kvTree();
-    }).is.throwing('Options are missing.');
-    done();
+  test('throws an error if service name is missing.', async () => {
+    await assert.that(async () => {
+      await kvTree({});
+    }).is.throwingAsync('Key is missing.');
   });
 
-  test('throws an error if service name is missing.', (done) => {
-    assert.that(() => {
-      kvTree({});
-    }).is.throwing('Key is missing.');
-    done();
-  });
-
-  test('calls callback when a key is changing', function (done) {
-    const key = `dc/home/${uuid()}/`;
-    let subKeyChanged = false;
-
+  test('emits changed event when a key is changing', async function () {
     this.timeout(10000);
-    consul.connect({
+
+    const key = `dc/home/${uuid()}/`;
+
+    await consul.connect({
       consulUrl: `http://${host}:8500`,
       serviceName: 'test',
       serviceUrl: `http://${host}:3000`,
       status: 'pass'
-    }, (errConnect) => {
-      assert.that(!errConnect || errConnect.message === 'Verification failed.').is.true();
-
-      consul.consul.kv.set({ key, value: '' }, (errSet, result) => {
-        assert.that(errSet).is.falsy();
-        assert.that(result).is.true();
-
-        consul.watchKv({
-          key
-        }, (errWatchTree, data) => {
-          assert.that(errWatchTree).is.null();
-
-          if (subKeyChanged) {
-            assert.that(data).is.ofType('array');
-            subKeyChanged = false;
-            return done();
-          }
-        });
-
-        consul.consul.kv.set({
-          key: `${key}sub/yy`,
-          value: 'huhu'
-        }, (errSet2, result2) => {
-          subKeyChanged = true;
-          assert.that(errSet2).is.falsy();
-          assert.that(result2).is.true();
-        });
-      });
     });
+
+    const result = await consul.consul.kv.set({ key, value: '' });
+
+    assert.that(result).is.true();
+
+    const watch = consul.watchKv({ key });
+    let subKeyChanged = false;
+
+    await Promise.all([
+      new Promise((resolve) => {
+        watch.on('change', (data) => {
+          if (!subKeyChanged) {
+            return;
+          }
+
+          assert.that(data).is.ofType('array');
+          resolve();
+        });
+      }),
+
+      (async () => {
+        const result2 = await consul.consul.kv.set({ key: `${key}sub/yy`, value: 'huhu' });
+
+        assert.that(result2).is.true();
+        subKeyChanged = true;
+      })()
+    ]);
   });
 });
